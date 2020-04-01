301b9a0232a28bcd8c87e48782dc18a95c152af8
555998 <hasanajardi@gmail.com>
2020-04-02 01:07:26 +0200

    Create Shqip.js

---
+++ .github/Shqip.js
@@ -0,0 +1,141 @@
+#EXTM3U
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Arta%20HD.png",Arta HD
+http://84.22.33.254/unicast.artanews/index.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/RTV%2021%20HD.png",RTV21 HD
+http://ott.artmott.com/Rtv21/index.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/KTV%20HD.png",KTV HD
+http://84.22.33.210/KtvHd/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/RTK%201.png", RTK 1
+http://ott.artmott.com/Rtk1/tracks-v1a1/mono.m3u8
+http://stream1.rtkit.com:1935/rtk1stream/rtk1.stream/chunklist_w228779816.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/RTK%203.png", RTK 3
+http://ott.artmott.com/Rtk3/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/RTK%204.png", RTK 4
+http://ott.artmott.com/Rtk4/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/T7.png",T7
+http://84.22.33.210/T7ott/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/TV%20Dukagjini.png",TV DUKAGJINI
+http://ott.artmott.com/Dukagjini/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Klan%20TV.png",KLAN HD
+http://84.22.33.218/tvKlan/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Klan%20Kosova.png",KLAN KOSOVA
+http://ott.artmott.com/KlanKosova/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Klan%20Plus.png",KLAN PLUS
+http://84.22.33.214/TVKlanPlus/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Alsat-M.png",Alsat-M
+http://ott.artmott.com/AlsatM/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/ERA%20TV.png",ERA TV
+http://ott.artmott.com/EraTV/index.m3u8
+#EXTINF:-1 tvg-logo="http://vignette.wikia.nocookie.net/logopedia/images/2/27/Logo_ok.png", VIZION PLUS
+http://ott.artmott.com/VizionPus/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Rrokum%20TV.png", Rrokum TV
+http://82.114.65.202:1935/live/rrokumtv/playlist.m3u8
+#EXTINF:0 tvg-logo="http://www.albepg.com/tvlogi/Zjarr%20TV.png", ZJARR TV
+http://217.73.132.66:1935/live/Zjarr.stream/playlist.m3u8
+#EXTINF:0 tvg-logo="http://www.albepg.com/tvlogi/Living%20HD.png", LIVING HD
+http://84.22.33.254/unicast.tringlivinghd/index.m3u8
+#EXTINF:0 tvg-logo="http://www.albepg.com/tvlogi/TV%20Opinion.png", Tv Opinion
+http://84.22.33.254/unicast.tvopinion/index.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/ALBUK%20TV.png",ALBUK TV
+http://albuk.dyndns.tv:1935/albuk/albuk.stream/playlist.m3u8
+#EXTINF:0 tvg-logo="http://www.albepg.com/tvlogi/PRIME.png", PRIME HD
+http://ott.artmott.com/Prime/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.tvboin.com/wp-content/uploads/2017/12/logo-modified-v1.png", BOIN TV
+http://tva.fastbroad.com:80/i/tvboin_1@380599/index_2000_av-p.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/EuroAL.png", EuroAL
+http://5.135.92.131:1935/live/euroAl/chunklist.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Muzik.png", Alb'swiss TV
+http://62.210.188.65:8181/hls/stream.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Zico%20TV.png", ZIKO TV
+http://ott.artmott.com/zico/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/MTV%20Kosova.png",MTV Kosova
+http://84.22.33.254/unicast.mtvkosova/index.m3u8
+http://ott.artmott.com/Rtk1/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="https://cdn.freebiesupply.com/logos/large/2x/turbo-1-logo-png-transparent.png", TURBO CHANNEL
+http://84.22.33.254/unicast.turbo/video.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/TV%20Opoja.png",TV Opoja
+https://54627d4fc5996.streamlock.net/tvopoja/tvopoja/chunklist_w1241215125.m3u8
+http://ip.opoja.tv:1935/tvopoja/tvopoja/playlist.m3u8
+#EXTINF:0 tvg-logo="http://www.albepg.com/tvlogi/Folk%20Plus.png", Folk Plus
+http://84.22.33.254/unicast.folkplus/video.m3u8
+#EXTINF:-1,TV ILIRIDA
+rtmps://54627d4fc5996.streamlock.net:443/rtvilirida/rtvilirida
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/BBF.png", BBF
+#http://84.22.33.254/unicast.bbf/index.m3u8
+http://82.114.67.178:8081/hls/PeaceTV.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Click%20TV.png", SPIN TV
+http://84.22.33.254/unicast.click/video.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Dasma%20TV.png", Dasma TV
+#http://ott.artmott.com/DasmaTV/tracks-v1a1/mono.m3u8
+#http://91.187.117.230:8080
+http://artiiptv.dyndns.tv:8090/ISTOGUCHANNEL/tracks-v1a1/mono.m3u8
+#EXTINF:0 tvg-logo="http://www.albepg.com/tvlogi/Film%20Nje%20HD.png", FILM NJE
+http://84.22.33.210/njehd.ott/tracks-v1a1/mono.m3u8
+#EXTINF:0 tvg-logo="http://www.albepg.com/tvlogi/Film%20Dy%20HD.png", FILM DY
+http://84.22.33.210/dyhd.ott/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Art%20Kino%201.png", Art Kino 1
+http://ott.artmott.com/ArtKino1/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Art%20Kino%202.png", Art Kino 2
+http://ott.artmott.com/ArtKino2/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Art%20Kino%203.png", Art Kino 3
+http://ott.artmott.com:90/ArtKino3/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Art%20Doku%201.png", Art Doku 1
+http://ott.artmott.com/ArtDoku1/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Art%20Doku%202.png", Art Doku 2
+http://ott.artmott.com/ArtDoku2/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Tring%20Action.png", TringAction
+http://ott.artmott.com/TringAction/tracks-v1a1/mono.m3u8
+#EXTINF:-1,TringFantasy
+http://ott.artmott.com/TringFantasy/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Tring%20Life.png", TringLife
+http://ott.artmott.com/TringLife/tracks-v1a1/mono.m3u8
+#EXTINF:-1,TringSuper
+http://ott.artmott.com:90/TringSuper/tracks-v1a1/mono.m3u8
+#EXTINF:-1,TringShqip
+http://ott.artmott.com/TringShqip/tracks-v1a1/mono.m3u8
+#EXTINF:-1,TringComedy
+http://ott.artmott.com/TringComedy/tracks-v1a1/mono.m3u8
+#EXTINF:-1,JollyHD
+http://ott.artmott.com/JollyHD/tracks-v1a1/mono.m3u8
+#EXTINF:-1,Smile
+http://ott.artmott.com/Smile/tracks-v1a1/mono.m3u8
+#EXTINF:-1,TringWorld
+http://ott.artmott.com/TringWorld/tracks-v1a1/mono.m3u8
+#EXTINF:-1,TringHistory
+http://ott.artmott.com/TringHistory/tracks-v1a1/mono.m3u8
+#EXTINF:-1,TringPlanet
+http://ott.artmott.com/TringPlanet/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/Tring%20Kids.png", TringKids
+http://ott.artmott.com/TringKids/tracks-v1a1/mono.m3u8
+#EXTINF:-1,TringTring
+http://ott.artmott.com/TringTring/tracks-v1a1/mono.m3u8
+#EXTINF:-1,TipTV
+http://ott.artmott.com/TipTV/tracks-v1a1/mono.m3u8
+#EXTINF:-1 tvg-logo="http://www.albepg.com/tvlogi/T%20HD.png",T HD
+http://ott.artmott.com/7HD/tracks-v1a1/mono.m3u8
+#EXTINF:-1,SkyStar
+http://ott.artmott.com/SkyStar/tracks-v1a1/mono.m3u8
+#EXTINF:-1,SKYACTION
+http://ott.artmott.com/SKYACTION/tracks-v1a1/mono.m3u8
+#EXTINF:-1,SKYCOMEDY
+http://ott.artmott.com/SKYCOMEDY/tracks-v1a1/mono.m3u8
+#EXTINF:-1,SKYMAX
+http://ott.artmott.com/SKYMAX/tracks-v1a1/mono.m3u8
+#EXTINF:-1,SKYShqip
+http://ott.artmott.com/SKYShqip/tracks-v1a1/mono.m3u8
+#EXTINF:-1,FIGHTBOX
+http://84.22.33.210/FIGHTBOX/tracks-v1a1/mono.m3u8
+#EXTINF:-1,TRT 1
+http://84.22.33.210/trt1/tracks-v1a1/mono.m3u8
+#EXTINF:-1,AtvHd
+http://ott.artmott.com/AtvHd/tracks-v1a1/mono.m3u8
+#EXTINF:-1,KanalD
+http://ott.artmott.com/KanalD/tracks-v1a1/mono.m3u8
+#EXTINF:-1,StarTvHd
+http://ott.artmott.com/StarTvHd/tracks-v1a1/mono.m3u8
+#EXTINF:-1,foxturkey
+http://84.22.33.218/foxturkey/tracks-v1a1/mono.m3u8
+#EXTINF:-1,ShowTv
+http://ott.artmott.com/ShowTv/tracks-v1a1/mono.m3u8
+#EXTINF:-1,MTVEVROPE
+http://84.22.33.254/unicast.mtveurope/tracks-v1a1/mono.m3u8
